f57a1ab2eb815103c4b74aa6ad2962bffe442aff
Ren Wang <iamwrm@gmail.com>
2017-08-01 15:01:59 +0800

    solution update: data trans algo keyper: dont change page

---
+++ part/auto/solution.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "solution"
+ (lambda ()
+   (TeX-add-symbols
+    "widthOfScalerStepFigure"
+    "widthOfMatcherFigure")
+   (LaTeX-add-labels
+    "indexScript"
+    "scalerStep0"
+    "scalerStep1"
+    "scalerStep2"
+    "matcherStep0"
+    "matcherStep1"))
+ :latex)
+
