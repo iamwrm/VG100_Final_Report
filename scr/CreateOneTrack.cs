f83cc4e1d768210126d22c7e1e77db65b2265c24
Ren Wang <iamwrm@gmail.com>
2017-08-06 10:56:08 +0800

    added code

--- scr/CreateOneTrack.cs
+++ scr/CreateOneTrack.cs
@@ -1,83 +1,44 @@
-	void CreateOneTrack(AudioSource AudioSourceInputed,
-						ref List<double> MusicScoreInputed,
-						int StartY,
-						ref int isAccRecordingLocal,
-						ref int InstrumentType,
-						ref int PitchingLocal)
+void CreateOneTrack(AudioSource AudioSourceInputed,
+					ref List<double> MusicScoreInputed,
+					int StartY, ref int isAccRecordingLocal,
+					ref int InstrumentType, ref int PitchingLocal)
 	{
-
-		ShowDouble(MusicScoreInputed, "SoureInputed: ", 0, 0 + StartY);
-
-		int x0 = 200;
-		int XGap = 150;
-		int YGap = 0;
-		int SizeX = 130;
-		int SizeY = 100;
-		if (GUI.Button(new Rect(x0 + 0, StartY, SizeX, SizeY), "Recorded"))
+	ShowDouble(MusicScoreInputed, "SoureInputed: ", 0, 0 + StartY);
+	int x0 = 200;
+	int XGap = 150;
+	int YGap = 0;
+	int SizeX = 130;
+	int SizeY = 100;
+	if (GUI.Button(new Rect(x0 + 0, StartY, SizeX, SizeY), "Recorded"))
+	{
+		for (int i = 0; i < musicScoreX.Count; i++)
 		{
-			for (int i = 0; i < musicScoreX.Count; i++)
-			{
-				MusicScoreInputed.Add(musicScoreX[i]);
-			}
-			musicScoreX.Clear();
-			//CombineScore(MusicScoreInputed, AudioSourceInputed, InstrumentType);
+			MusicScoreInputed.Add(musicScoreX[i]);
 		}
-
-		if (GUI.Button(new Rect(x0 + XGap, StartY + YGap, SizeX, SizeY), "Play music"))
+		musicScoreX.Clear();
+	}
+	if (GUI.Button(new Rect(x0 + XGap, StartY + YGap, SizeX, SizeY), "Play music"))
+	{
+		CombineScore(MusicScoreInputed, AudioSourceInputed, InstrumentType);
+		if (!AudioSourceInputed.isPlaying)
 		{
+			AudioSourceInputed.Play();
+		}
+	}
+	if (GUI.Button(new Rect(x0 + 2 * XGap, StartY + YGap, SizeX / 2, SizeY), "Clear"))
+	{
+		MusicScoreInputed.Clear();
+	}
+	int InstrumentControlx0 = 20;
+	int InstrumentControly0 = StartY + 20;
+	if (GUI.Button(new Rect(InstrumentControlx0, InstrumentControly0, 
+	SizeX / 3, SizeY - 30), "1"))
+	{
+		InstrumentType = 2;
+	}
 
-			CombineScore(MusicScoreInputed, AudioSourceInputed, InstrumentType);
-			if (!AudioSourceInputed.isPlaying)
-			{
-				AudioSourceInputed.Play();
-			}
-		}
-
-		if (GUI.Button(new Rect(x0 + 2 * XGap, StartY + YGap, SizeX / 2, SizeY), "Clear"))
-		{
-			MusicScoreInputed.Clear();
-		}
-
-		int InstrumentControlx0 = 20;
-		int InstrumentControly0 = StartY + 20;
-		if (GUI.Button(new Rect(InstrumentControlx0, InstrumentControly0, SizeX / 3, SizeY - 30), "1"))
-		{
-			InstrumentType = 2;
-		}
-		if (GUI.Button(new Rect(InstrumentControlx0 + SizeX / 3 + 10, InstrumentControly0, SizeX / 3, SizeY - 30), "2"))
-		{
-			InstrumentType = 3;
-		}
-		if (GUI.Button(new Rect(InstrumentControlx0 + SizeX / 3 + 70, InstrumentControly0, SizeX / 3, SizeY - 30), "3"))
-		{
-			InstrumentType = 4;
-		}
-
-		int PitchingControlx0 = 0;
-		int PitchingControly0 = StartY + 110;
-		if (GUI.Button(new Rect(PitchingControlx0, PitchingControly0, 150, 50), "Toggle Pitching"))
-		{
-			if (PitchingLocal == 0)
-			{
-				PitchingLocal = 1;
-				AudioSourceInputed.loop = true;
-			}
-			else
-			{
-				PitchingLocal = 0;
-				AudioSourceInputed.loop = false;
-			}
-			Debug.Log(PitchingLocal);
-		}
-
-		int PitchingSlidex0 = PitchingControlx0 + 200;
-		int PitchingSlidey0 = PitchingControly0 + 30;
-		if (PitchingLocal == 1)
-		{
-			float musicPitch;
-			musicPitch = GUI.HorizontalSlider(new Rect(PitchingSlidex0, PitchingSlidey0, 300, 50),
-			(float)(System.Math.Pow(2.9, Input.acceleration.x)), 0.0F, 3.0F);
-			AudioSourceInputed.pitch = musicPitch;
-		}
-
+	if (GUI.Button(new Rect(InstrumentControlx0 + SizeX / 3 + 10,
+	 InstrumentControly0, SizeX / 3, SizeY - 30), "2"))
+	{ 
+		InstrumentType = 3; 
 	}
---
+++ scr/pm2.cs
@@ -0,0 +1,45 @@
+	if (GUI.Button(new Rect(600, 1050, 150, 150), "Change Scene to 1")) {
+		SceneManager.LoadScene(1); 
+	}
+	if (GUI.Button(new Rect(600, 1200, 150, 150), "Change Scene to 2")) {
+		SceneManager.LoadScene(2); 
+	}
+	for (int i = 0; i < 8; i++) {
+		if (GUI.Button(new Rect(60 * i, 650 + 50, 55, 80), (i).ToString())) {
+			musicScoreX.Add(i);
+		}
+	}
+	Vector3 nowDev = Input.acceleration - InitialGravityAccForX;
+	int aa, ab, ac;
+	aa = nowDev.x >= 0 ? 1 : 0;
+	ab = nowDev.y >= 0 ? 1 : 0;
+	ac = nowDev.z >= 1 ? 1 : 0;
+	int[] WRmap = { 3, 4, 2, 1, 0, 7, 5, 6 };
+	if (GUI.Button(new Rect(400, 800, 150, 150), " Start Motion")) {
+		RecordingX = 1;
+	}
+	if (GUI.Button(new Rect(400 + 150, 800, 150 / 2, 150), " Stop")) {
+		RecordingX = 0;
+	}
+	if (RecordingX == 1) {
+		if ((nowDev).magnitude > 0.5) {
+			if (!audioSourceX.isPlaying) {
+				audioSourceX.clip = 
+				AudioClipColletion[WRmap[aa + ab * 2 + ac * 4]];
+				musicScoreX.Add(WRmap[aa + ab * 2 + ac * 4]);
+				audioSourceX.Play();
+			}
+		}
+	}
+	if (GUI.Button(new Rect(400 - 50, 800 - 300, 130 + 50, 100), "Play Together")) {
+		if ((!audioSource1.isPlaying) && (!audioSource2.isPlaying) 
+			&& (!audioSource3.isPlaying)) {
+			ctrlWR1.control.audioSource1.Play();
+			ctrlWR1.control.audioSource2.Play();
+			ctrlWR1.control.audioSource3.Play();
+			ctrlWR.control.audioSource1.Play();
+			ctrlWR.control.audioSource2.Play();
+			ctrlWR.control.audioSource3.Play();
+		}
+	}
+}
